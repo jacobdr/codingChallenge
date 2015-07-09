d827f23735c0d2bb6268443eb58740f272a4fde6
jacobdr <jacobdr@gmail.com>
2015-07-09 00:55:02 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name="codingChallenge",
     version="0.0.1",
     author="Jacob Roberts",
     author_email="jacobdr@gmail.com",
-    description="Hoping to win the challenge with this submission that analyzes \
-        Twitter information"
+    url="INSERT_HERE",
+    description="Hoping to win the challenge with this submission \
+        that should run fast",
+    packages=["codingChallenge"],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
